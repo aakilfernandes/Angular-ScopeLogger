d099e09fd509afc04785c027603962acc6fa61e9
codefalling <codefalling@codefalngdeMini.lan>
2016-08-09 15:34:19 +0800

    make it works with webpack-dev-server

--- inject/listener.js
+++ inject/listener.js
@@ -4,31 +4,39 @@
 
 
 function bootstrap() {
-
-	var element = null
-
-	document.addEventListener("mousedown", function(event){
-	    if(event.button == 2)
+    if (window.iframe) {
+        iframe.addEventListener('load', function(){
+            bootstrap();
+        });
+    }
+    var element = null;
+    var doc = document;
+    var win = window;
+    if (window.iframe) {
+        doc = iframe.contentDocument;
+        win = iframe.contentWindow;
+    }
+    doc.addEventListener("mousedown", function(event){
+        if(event.button == 2)
 		    element = event.target;
 		else
 			element = null
 	}, true);
 
 	document.addEventListener('inspectscope',function(){
-		if(typeof angular === 'undefined'){
+        if(typeof win.angular === 'undefined'){
 			console.error('Cannot inspect scope, angular is undefined')
 			return
 		}
 
-		console.log(angular.element(element).scope())
-	})
+        console.log(win.angular.element(element).scope())
+    });
 
 }
 
 if (document.readyState === 'complete') {
-	bootstrap();
+    bootstrap();
 } else {
-	window.addEventListener('load', bootstrap);
+    window.addEventListener('load', bootstrap);
 }
-
 })(window);
